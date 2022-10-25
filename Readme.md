6ac6d811bc8d30dd0029ac572e6f297e66c24dc6
Kyuwoong Lee <kwooler@gmail.com>
2022-10-24 23:01:19 -0700

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+
+[Link to Chap 5 Assignment 2](https://docs.google.com/presentation/d/1r3h2R9JwK9HK_U2Ia-zncL0BSjHV6Giu6ugNJ6yZpgc/edit#slide=id.g12411e7f99b_0_21)
+
+![A02](https://nimbus-screenshots.s3.amazonaws.com/s/d7b3c05f4a3bea6f4db4e2a691b3aed1.png)
+
+## Compete the "main.py"
+
+
---
+++ main.py
@@ -0,0 +1,25 @@
+def isPrime(num):
+    ##################################################
+    # make your code
+    ##################################################
+    return 0
+
+
+def primeNumbers(begin, end):
+    ##################################################
+    # make your code
+    ##################################################
+    # isPrime(value)
+
+    return plist
+
+
+begin = 0
+end = 20
+rlst = primeNumbers(begin, end)
+print(rlst)
+
+begin = 10
+end = 50
+rlst = primeNumbers(begin, end)
+print(rlst)
