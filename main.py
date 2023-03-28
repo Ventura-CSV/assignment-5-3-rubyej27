4c176dd48267c303d1ea9750f838bb2401c46457
Kyu Lee <kwooler@gmail.com>
2023-03-27 23:07:18 -0700

    A53

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ def shiftN(stringvalue, direction, N):
     return stringvalue
 
 
-str = '001100'
-print(int(str, 2))
-rstr = shiftN(str, 0, 2)
-print(rstr)
-print(int(rstr, 2))
+def main():
+    str = '001100'
+    print(int(str, 2))
+    rstr = shiftN(str, 0, 2)
+    print(rstr)
+    print(int(rstr, 2))
 
-rstr = '110000'
-rstr = shiftN(rstr, 1, 4)
-print(rstr)
-print(int(rstr, 2))
+    rstr = '110000'
+    rstr = shiftN(rstr, 1, 4)
+    print(rstr)
+    print(int(rstr, 2))
+
+
+if __name__ == '__main__':
+    main()
